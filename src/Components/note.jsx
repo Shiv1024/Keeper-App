33a76de62137100ed6af6f0c2ca44e0ebff84c3c
Shiv1024 <120778356+Shiv1024@users.noreply.github.com>
2024-03-17 13:57:41 +0530

    Changes done again

--- src/Components/note.jsx
+++ src/Components/note.jsx
@@ -7,7 +7,7 @@ function Note(props){
     return(<div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        <button onClick={handlechange}>DELETE IT</button>
+        <button onClick={handlechange}>DELETE</button>
     </div>)
 }
 export default Note;
